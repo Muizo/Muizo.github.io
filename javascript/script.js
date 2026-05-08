062634506bb114d4c95e8fcd0917fed70531fc95
Abdulmuiz Sulaiman <dsulaiman12300@gmail.com>
2026-05-08 01:23:48 -0400

    Edited html documents and addes some Javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,25 @@
-function onMouse() {
-    document.getElementById("serious").style.color = "#fff";
-}
+//Contact form Validation
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
 
-function clickButton() {
-    document.getElementById("btn").style.display = "block";
-}
+    form.addEventListener("submit", (event) =>{
+        event.preventDefault();
+
+        const fullName = document.getElementById("fullName").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const subject = document.getElementById("subject").value.trim();
+        const message = document.getElementById("message").value.trim();
+
+        if (!fullName || !email || !subject || !message) {
+            alert("please fill in all fields.");
+            return;
+        }
+
+        else {
+            alert ("message sent sucessfully!");
+        }
+        console.log({fullName, email, subject, message})
+        form.rest();
+    });
+})
+
